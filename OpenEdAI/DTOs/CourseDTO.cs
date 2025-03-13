34b30e699cc54a33d7f0143b03c30ab722bc488d
Morganiron <86537433+Morganiron@users.noreply.github.com>
2025-03-13 15:27:47 -0500

    Task D - Added [Required] attribute to required properties for verfication and testing

--- OpenEdAI/DTOs/CourseDTO.cs
+++ OpenEdAI/DTOs/CourseDTO.cs
@@ -1,12 +1,18 @@
-﻿namespace OpenEdAI.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OpenEdAI.DTOs
 {
     public class CourseDTO
     {
+        [Required]
         public int CourseID { get;  set; }
+        [Required]
         public string Title { get;  set; }
         public string Description { get; set; }
         public List<string> Tags { get;  set; }
+        [Required]
         public string UserID { get;  set; } // Creator's ID
+        [Required]
         public string UserName { get;  set; }
         public DateTime CreatedDate { get;  set; }
         public DateTime UpdateDate { get;  set; }
@@ -18,7 +24,9 @@
 
     public class EnrolledStudentDTO
     {
+        [Required]
         public string UserID { get; set; }
+        [Required]
         public string UserName { get; set; }
     }
 }
