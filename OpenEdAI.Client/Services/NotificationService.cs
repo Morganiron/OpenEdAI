e117b3dc9303c8285d0abed104fa003820148c2d
Morganiron <86537433+Morganiron@users.noreply.github.com>
2025-04-18 18:43:07 -0500

    Task B - feat(ux/ui): add notification popup for course submission
    - Changed NotificationService with NotifyAndAwait/Acknowledge to allow for confirmation
    - Updated MainLayout to show popup with OK button
    - Refactored methods in MainLayout to use proper setup for new notification
    - Updated App.razor to await NotifyAndAwait before navigating
    - Updated login button on LandingPage to use the same button style as the others.

--- OpenEdAI.Client/Services/NotificationService.cs
+++ OpenEdAI.Client/Services/NotificationService.cs
@@ -2,11 +2,31 @@
 {
     public class NotificationService
     {
+        // This event is triggered when a notification is sent
         public event Action<string>? OnNotify;
 
-        public void Notify(string message)
+        // This event is triggered when the user acknowledges a notification
+        public event Action? OnAcknowledge;
+
+        public Task NotifyAndAwait(string message)
         {
+            var tcs = new TaskCompletionSource<bool>();
+
+            // When the layout invokes Ackknowledge, we complete the task
+            void Handler()
+            {
+                OnAcknowledge -= Handler;
+                tcs.SetResult(true);
+            }
+            OnAcknowledge += Handler;
+            // Trigger the display
             OnNotify?.Invoke(message);
+            return tcs.Task;
+        }
+        // Called when the user acknowledges the notification
+        public void Acknowledge()
+        {
+            OnAcknowledge?.Invoke();
         }
     }
 }
